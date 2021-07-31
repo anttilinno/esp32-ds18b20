bfcd0c4de03d1dbe859c5ad0dd8fce500ae64a2c
anttilinno <antti.linno@gmail.com>
2021-07-31 14:27:28 +0300

    Update main.py
    Ran prettify on html, added page autorefresh and refresh button.

--- main.py
+++ main.py
@@ -14,18 +14,35 @@ def read_ds_sensor():
  
 def html_page():
   temp = read_ds_sensor()
-  html = """<!DOCTYPE HTML><html><head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-  <style> html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center; }
-    h2 { font-size: 3.0rem; } p { font-size: 3.0rem; } .units { font-size: 1.2rem; } 
-    .ds-labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px; }
-  </style></head><body><h2>ESP32 DS18B20 WebServer</h2>
-  <p><i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="ds-labels">Temperature</span>
-    <span id="temperature">""" + str(temp) + """</span>
-    <sup class="units">&deg;C</sup>
-  </p></body></html>"""
+  html = """
+    <!DOCTYPE HTML>
+    <html>
+
+    <head>
+        <meta http-equiv="refresh" content="30"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <style>
+            html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center; }
+                h2 { font-size: 3.0rem; } p { font-size: 3.0rem; } .units { font-size: 1.2rem; } 
+                .ds-labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px; }
+        </style>
+    </head>
+
+    <body>
+        <h2>ESP32 DS18B20 WebServer</h2>
+        <p><i class="fas fa-thermometer-half" style="color:#059e8a;"></i>
+            <span class="ds-labels">Temperature</span>
+            <span id="temperature">""" + str(temp) + """</span>
+            <sup class="units">&deg;C</sup>
+        </p>
+        <p>
+		      <button onClick="location.reload()">Refresh</button>
+	      </p>
+    </body>
+
+    </html>
+  """
   return html
 
 def json_page():
